7e8964b8fd195f37f9791947b90f2c04ee9cdcec
RDB-Biller <ripodatabase@gmail.com>
2026-03-25 03:54:49 +0000

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,8 +25,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-import cors from "cors";
-
 app.use(cors({
-  origin: "*"
+  origin: "https://insurexphome.vercel.app/"
 }));
